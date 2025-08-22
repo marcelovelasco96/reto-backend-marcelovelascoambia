3c72fef54dbe8f273a77b97f2191d5bb7362912c
Marcelo <marcelovelasco69@gmail.com>
2025-08-22 02:20:04 -0500

    Add helper script run.ps1

---
+++ run.ps1
@@ -0,0 +1,10 @@
+﻿param(
+  [int]$Port = 8080
+)
+
+Write-Host "-> Levantando order-service en puerto $Port ..."
+# Si en application.yml usas la variable SERVER_PORT, la seteamos por si acaso
+$env:SERVER_PORT = "$Port"
+
+# Forzamos también por argumento (manda sobre yml)
+.\mvnw.cmd spring-boot:run -D"spring-boot.run.arguments=--server.port=$Port"
